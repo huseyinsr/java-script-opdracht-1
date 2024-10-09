90bdfbe5b03348281f309cc599375fcc65195d5d
huseyinsr <39071@ma-web.nl>
2024-10-09 12:10:42 +0200

    Add files via upload

---
+++ less.js
@@ -0,0 +1,25 @@
+// Spelerinformatie
+let name = "Erwin Henraat";
+let jumpforce = 20;  // Number
+let specialPower = "teleport";  // Text
+let speed = 280;  // Number
+let bike = "Honda VFR VTEC";  // Text
+let livesLeft = 1;  // Number
+let ammo = 121;  // Number
+let numberOfLasers = 5;  // Number
+let batteryPower = 90;  // Number
+let level = 9000;  // Number
+let wanted = true;  // Boolean
+
+// Output
+console.log("name: " + name);
+console.log("jumpforce: " + jumpforce);
+console.log("special power: " + specialPower);
+console.log("speed: " + speed);
+console.log("bike: " + bike);
+console.log("lives left: " + livesLeft);
+console.log("ammo: " + ammo);
+console.log("number of lasers: " + numberOfLasers);
+console.log("battery power: " + batteryPower);
+console.log("level: " + level);
+console.log("wanted: " + wanted);
